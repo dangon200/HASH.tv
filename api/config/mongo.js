6188258ec81e18325260a41b7892ab18ae999a82
Haru <frankcito639@gmail.com>
2022-11-21 13:49:27 -0500

    models, Rols, Stream, Users

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
@@ -20,24 +19,3 @@ const dbConnect = () => {
 };
 
 module.exports = dbConnect;
-=======
-const mongoose = require("mongoose")
-
-const dbConnect = () =>{
-
-    const DB_URI = process.env.DB_URI;
-    mongoose.connect(DB_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    },(err, res)=>{
-        if(!err){
-            console.log("*******CONECCION EXITOSA***********")
-        }else{
-            console.log("********ERROR DE CONECCION*********")
-        }
-    })
-
-}
-
-module.exports = dbConnect
->>>>>>> 8f938ffda83711ccc0ce08381fa7abe2d1af7e41
