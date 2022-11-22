1859c0584e0498eedc9b8ffa82e0f03a1534fdc8
dangon200 <gonzalezdaniel230@gmail.com>
2022-11-22 10:06:51 -0300

    navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import './NavBar.css';
 import { IconContext } from 'react-icons';
 import logo from '../../utils/logo.svg'
 import SeachBar from '../searchBar/searchBar'
-  
+
 function Navbar() {
     const [sidebar, setSidebar] = useState(false);
   
