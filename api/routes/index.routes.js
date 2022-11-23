4b80b329a8cf181ea19c7e36b10a3cd7e4d28aa0
Haru <frankcito639@gmail.com>
2022-11-22 20:29:41 -0500

    Arreglando el merch

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,23 +1,12 @@
 const { Router } = require("express");
-const authRoutes = require("./auth.routes");
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 9fa96d5d355308ca355a30a820a404b91eb29ded
 const router = Router();
 
+const authRoutes = require("./auth.routes");
 const users = require("./users.routes");
 
 router.use("/", users);
 router.use("/api/auth", authRoutes);
-<<<<<<< HEAD
-=======
-const users = require("./users.routes")
-const router = Router();
 
-router.use("/", users)
-
->>>>>>> 9fa96d5d355308ca355a30a820a404b91eb29ded
+router.use("/", users);
 
 module.exports = router;
