970d47bf3274720f29f4959a146ba42abdae515a
Franco Nicolas <franconicolas2009@live.com.ar>
2022-11-23 11:25:52 -0300

    ruta Delete

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,12 +1,9 @@
 const { Router } = require("express");
 
-const signin = require("./signin.routes");
-const signout = require("./signout.routes");
 const users = require("./users.routes")
 const router = Router();
 
-router.use("/", signin);
-router.use("/", signout);
+
 router.use("/", users)
 
 module.exports = router;
