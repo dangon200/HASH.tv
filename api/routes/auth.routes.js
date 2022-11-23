d0cee9569c7e353dc5b44ea5f3ee144540c8c41c
Franco Nicolas <franconicolas2009@live.com.ar>
2022-11-23 18:18:50 -0300

    Rutas Stream

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -4,7 +4,6 @@ const router = Express.Router();
 const { signUp, signIn } = require("../controllers/auth.controller");
 
 router.post("/signup", signUp);
-
 router.post("/signin", signIn);
 
 module.exports = router;
--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -3,8 +3,10 @@ const router = Router();
 
 const authRoutes = require("./auth.routes");
 const users = require("./users.routes");
+const streams = require("./streams.routes")
 
-router.use("/", users);
+router.use("/api", users);
 router.use("/api/auth", authRoutes);
+router.use("/api", streams);
 
 module.exports = router;
