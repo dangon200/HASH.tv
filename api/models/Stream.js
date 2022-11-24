0e0b59f00611db939dac6bbab7cdb070863d22ce
Franco Nicolas <franconicolas2009@live.com.ar>
2022-11-24 16:54:46 -0300

    cambios modelo Stream

--- api/models/Stream.js
+++ api/models/Stream.js
@@ -1,20 +1,39 @@
+const {Categories} = require("../models//Categories")
 const { Schema, model } = require("mongoose");
 
 const StreamSchema = new Schema(
   {
+  image:{
+    type: String
+  },
   name: 
   {type:String
   },
+  user:{
+    type:String
+  },
   category: 
-    {type:String
-    }, ////  CREAR relacion Categorias
+    { type: String
+    }
+    , ////  CREAR relacion Categorias
   description:{
     type:String
   },
-  subcriptores:
-    {type: String
-
-    } ///  CRAR relacion con Subcriptores
+  subcriptores:[
+    {
+      type: Schema.ObjectId,
+      ref: "Suscribers",
+    },
+  ], ///  CRAR relacion con Subcriptores
+  rules:{
+    type:String
+  },  
+  networks:{
+    type:String
+  },
+  contents:{
+    type:String
+  }
 },
 {
   timestamps: true,
