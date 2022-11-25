da5e7b660f6f0e302040a7f352a848779b0b740b
Agustin Fraile <agustinfraile98@gmail.com>
2022-11-25 20:36:14 -0300

    agregando estilos al componente details

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -16,27 +16,56 @@ const Detail = (props) => {
     }, [])
 
     return (
-        <div>
+        <div className={s.cnt}>
             <Navbar />
-            <Link to="/">
-                <button>Back</button>
-            </Link>
-            <Link to="/user">
-                <button>User</button>
-            </Link>
+
 
             {streamDetail[0] && (
-                <div>
-                    <img src={streamDetail[0].image} className={s.img}/>
-                    <p>Name: {streamDetail[0].name}</p>
-                    <p>User: {streamDetail[0].user}</p>
-                    <p>Category: {streamDetail[0].category}</p>
-                    <p>Description: {streamDetail[0].description}</p>
-                    <p>Suscriptores: {streamDetail[0].subcriptores}</p>
-                    <p>Rules: {streamDetail[0].rules}</p>
-                    <p>Network: {streamDetail[0].networks}</p>
-                    <p>Contents: {streamDetail[0].contents}</p>
-                </div>)}
+                
+                <div className={s.detail_cnt}>
+
+                    <Link 
+                        to="/user" 
+                        style={{textDecoration:'none'}}
+                        title={`Ir al perfil de ${streamDetail[0].name}`}
+                    >
+                        <div className={s.detail_cnt_user}>
+                            <div className={s.detail_cnt_user__img}>
+                                <img src={streamDetail[0].image} className={s.img} />
+                            </div>
+                            <div className={s.detail_cnt_user__name}>
+                                <p>Name: {streamDetail[0].name}</p>
+                                <p>@{streamDetail[0].user}</p>
+                            </div>
+                        </div>
+                    </Link>
+
+                    <div className={s.detail_cnt_info}>
+                        <div className={s.detail_cnt_info__list}>
+                            <p>Category: {streamDetail[0].category}</p>
+                        </div>
+                        <div className={s.detail_cnt_info__list}>
+                            <p>{streamDetail[0].description}</p>
+                        </div>
+                        <div className={s.detail_cnt_info__list}>
+                            <p>Suscriptores: {streamDetail[0].subcriptores}</p>
+                        </div>
+                        <div className={s.detail_cnt_info__list}>
+                            <p>Rules: {streamDetail[0].rules}</p>
+                        </div>
+                        <div className={s.detail_cnt_info__list}>
+                            <p>Network: {streamDetail[0].networks}</p>
+                        </div>
+                        <p>Contents: {streamDetail[0].contents}</p>
+                    </div>
+                
+                    <Link to="/">
+                        <button className={s.button_back}>Back</button>
+                    </Link>
+            
+                </div>
+                                   
+            )}
         </div>
     )
 }
