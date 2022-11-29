2d7e9a55217e37bd768bc956c1d74a91c61f87d6
Haru <frankcito639@gmail.com>
2022-11-29 12:03:47 -0500

    model Subscriptions

--- api/models/Stream.js
+++ api/models/Stream.js
@@ -2,58 +2,69 @@ const { Schema, model } = require("mongoose");
 
 const StreamSchema = new Schema(
   {
-    title:{
-      type:String
-    },
-    name: 
-    {type:String, 
- 
-    },
-  image:{
-    type: String
-  },
-  user:{
-    type:String
-  },
-  category:[
-    {
+    title: {
       type: String,
-      
     },
-  ]
-    , ////  CREAR relacion Categorias
-  description:{
-    type:String
-  },
-  subcriptores:[
-    {
-     type:String
+    name: { type: String },
+    image: {
+      type: String,
     },
-  ], ///  CRAR relacion con Subcriptores
-  rules:{
-    type:String
-  },  
-  networks:{
-    type:String
+    user: {
+      type: String,
+    },
+    category: [
+      {
+        type: String,
+      },
+    ], ////  CREAR relacion Categorias
+    description: {
+      type: String,
+    },
+    subcriptores: [
+      {
+        type: String,
+      },
+    ], ///  CRAR relacion con Subcriptores
+    rules: {
+      type: String,
+    },
+    networks: {
+      type: String,
+    },
+    contents: {
+      type: String,
+    },
+    language: {
+      type: [
+        "Inglés",
+        "Chino mandarín",
+        "Hindi",
+        "Español",
+        "Francés",
+        "Ruso",
+        "Portugués",
+        "Alemán",
+        "Japonés",
+      ],
+      default: "Español",
+    },
+    continent: {
+      type: [
+        "África",
+        "Antártida",
+        "Asia",
+        "Europa",
+        "América del Norte",
+        "Oceanía",
+        "América del Sur",
+      ],
+      default: "América del Sur",
+    },
   },
-  contents:{
-    type:String
-  },
-  language:{
-    type: ['Inglés', 'Chino mandarín', 'Hindi', 'Español', 'Francés', 'Ruso', 'Portugués', 'Alemán', 'Japonés' ], default: "Español"
-  },
-  continent:{
-    type:["África", "Antártida", "Asia", "Europa", "América del Norte", "Oceanía", "América del Sur"], default: "América del Sur"
-  },
-},
-{
-  timestamps: true,
-  versionKey: false,
-}
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
-
-// const Streams = model("Streams", StreamSchema);
-
-// module.exports = {Streams};
 
 module.exports = model("Streams", StreamSchema);
