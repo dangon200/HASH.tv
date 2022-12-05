6545b069e154ae67dd986bcf5b42df0fe0dcdbb3
Agustin Fraile <agustinfraile98@gmail.com>
2022-12-05 10:48:12 -0300

    terminando estilos de stream

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -16,43 +16,50 @@ const Detail = (props) => {
     }, [])
 
     return (
-        <div>
-            <LinkContainer>
-                <StyledLink to="/">Back</StyledLink>
-                <span style={{ fontWeight: '700', fontSize: '20px' }}>CONTENIDO EN VIVO</span>
-                {/* <StyledLink to={`/user`}>Mi Perfil</StyledLink> */}
-            </LinkContainer>
-            <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%', marginTop: '10px', height: '88vh' }}>
-                <div style={{
-                    width: '15%',
-                    backgroundColor: '#E3E5E5',
-                    borderRadius: '16px',
-                    marginBottom: '20px',
-                    height: '88vh',
-                }}
-                >
-                    <p><b>CANALES RECOMENDADOS</b></p>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '10px', overflowY: 'scroll', height: '78vh' }}>
-                        {[0, 1, 2, 3, 4, 5, 6].map((e => {
-                            return (
-                                <div key={e} style={{
-                                    display: 'flex',
-                                }}
-                                >
-                                    <img src={streamDetail[0]?.image} className={s.img} />
-                                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', padding: '10px 0', textAlign: 'left' }}>
-                                        <span style={{ margin: '0' }}><b>{streamDetail[0]?.name}</b> @{streamDetail[0]?.user}</span>
-                                        <span style={{ margin: '0' }}>{streamDetail[0]?.category || 'Sin Categoría'}</span>
-                                        <span style={{ margin: '0' }}>{streamDetail[0]?.subcriptores || '0'} subs</span>
-                                        <span style={{ margin: '0' }}><span style={{ color: '#2ce02c', margin: '0' }}>● </span>9999</span>
+        <div className={s.container}>
+            <div className={s.linkContainer}>
+                <Link className={s.linkContainer_button_link} to="/">
+                    <button className={s.linkContainer_button}>Back</button>
+                </Link>
+                <div className={s.linkContainer_container}>
+                    <h3>Contenido en vivo</h3>
+                </div>
+            </div>
+
+            <div className={s.infoContainer} >
+                <div className={s.infoContainer_channels} >
+                    <p>Canales recomendados</p>
+                    <div className={s.infoContainer_channels_img} >
+                        {
+                            [0, 1, 2, 3, 4, 5, 6].map((e => {
+                                return (
+                                    <div className={s.img_box} key={e} >
+                                        <img src={streamDetail[0]?.image} className={s.img} />
+                                        <div className={s.img_info} >
+                                            <div className={s.img_detail_user}>
+                                                <p>{streamDetail[0]?.name}</p> 
+                                                <p>@{streamDetail[0]?.user}</p>
+                                            </div>
+                                            <div className={s.img_detail_category}>  
+                                                <p>{streamDetail[0]?.category || 'Sin Categoría'}</p>
+                                            </div>
+                                            <div className={s.detail_sub}>
+                                                <p>{streamDetail[0]?.subcriptores || '0'} subs</p>
+                                            </div>
+                                            <div className={s.detail_watch}>
+                                                <p 
+                                                style={{ color: '#2ce02c', margin: '0' }}>● 9999</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        }))}
+                                )
+                            }))
+                        }
                     </div>
                 </div>
                 <div style={{ width: '60%' }}>
-                    <div style={{ backgroundColor: 'black', width: '100%', height: '43vh', borderRadius: '16px', position: 'relative', marginBottom: '10px' }}>
+                    <div 
+                        style={{ backgroundColor: 'black', width: '100%', height: '43vh', borderRadius: '16px', position: 'relative', marginBottom: '10px' }}>
                         <div style={{
                             color: 'white',
                             backgroundColor: 'red',
@@ -68,83 +75,59 @@ const Detail = (props) => {
                             lineHeight: '30px',
                             fontWeight: '700',
                         }}
-                        >
-                            EN DIRECTO
-                        </div>
-                    </div>
-                    <div style={{
-                        boxSizing: 'border-box',
-                        backgroundColor: '#E3E5E5',
-                        width: '100%',
-                        borderRadius: '16px',
-                        padding: '20px',
-                        marginBottom: '10px',
-                        height: '43vh',
-                    }}
                     >
+                            EN DIRECTO
+                    </div>
+                    </div>
+
+                    <div className={s.streamer_cnt}>
                         {streamDetail[0] && (
-                            <div>
-                                <div style={{
-                                    display: 'flex',
-                                }}
-                                >
+                            <div className={s.streamer_cnt_info}>
+                                <div className={s.streamer_cnt_info_user} >
                                     <img src={streamDetail[0].image} className={s.img} />
-                                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', padding: '10px 0', textAlign: 'left' }}>
-                                        <span style={{ margin: '0' }}><b>{streamDetail[0].name}</b> @{streamDetail[0].user}</span>
-                                        <span style={{ margin: '0' }}>{streamDetail[0].category || 'Sin Categoría'}</span>
-                                        <span style={{ margin: '0' }}>{streamDetail[0].subcriptores || '0'} subs</span>
-                                        <span style={{ margin: '0' }}><span style={{ color: '#2ce02c', margin: '0' }}>● </span>9999</span>
+                                    <div className={s.streamer_cnt_info_detail}>
+                                    <div style={{ margin: '0' }}>
+                                            <b>{streamDetail[0].name}</b> @{streamDetail[0].user}
+                                        </div>
+                                        <div style={{ margin: '0' }}>
+                                            {streamDetail[0].category || 'Sin Categoría'}
+                                        </div>
+                                        <div style={{ margin: '0' }}>
+                                            {streamDetail[0].subcriptores || '0'} subs
+                                        </div>
+                                        <div style={{ margin: '0' }}>
+                                            <span style={{ color: '#2ce02c', margin: '0' }}>● </span>9999
+                                        </div>
+                                    </div>  
+                                </div>
+                                <div className={s.streamer_desc}>
+                                    <div className={s.streamer_desc_about}>
+                                       <p className={s.streamer_bgc_red}>Acerca de {streamDetail[0].description}</p>
+                                    </div>
+                                    <div className={s.streamer_desc_rules}>
+                                        <p><span className={s.streamer_bgc_blue}>Reglas:</span> {streamDetail[0].rules}</p>
+                                    </div>
+                                    <div className={s.streamer_desc_net}>
+                                        <p><span className={s.streamer_bgc_blue}>Network:</span> {streamDetail[0].networks}</p>
+                                    </div>
+                                    <div className={s.streamer_desc_content}>
+                                        <p><span className={s.streamer_bgc_blue}>Contents:</span> {streamDetail[0].contents}</p>
                                     </div>
                                 </div>
-                                <p>{streamDetail[0].description}</p>
-                                <p><b>Reglas:</b> {streamDetail[0].rules}</p>
-                                <p>Network: {streamDetail[0].networks}</p>
-                                <p>Contents: {streamDetail[0].contents}</p>
-                            </div>)}
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div style={{
-                    width: '15%',
-                    backgroundColor: '#E3E5E5',
-                    borderRadius: '16px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    height: '88vh'
-                }}
-                >
-                    <p style={{ fontWeight: '700' }}>CHAT EN VIVO</p>
-                    <div style={{ backgroundColor: '#FFFFFF', width: '90%', height: '77vh', borderRadius: '16px', marginBottom: '10px' }}></div>
-                    <div style={{ backgroundColor: '#FFFFFF', width: '90%', height: '32px', borderRadius: '16px' }}>
-                        <input style={{ border: 'none', outline: 'none', lineHeight: '30px', width: '75%', height: '30px' }} />
-                    </div>
+
+                <div className={s.chat} >
+                    <p>Chat en vivo</p>
+                    <div className={s.chat_cnt}></div>
+                    <input type="text" className={s.chat_input} placeholder='Escribe el mensaje'/>
                 </div>
             </div>
         </div>
     )
 }
 
-const StyledLink = styled(Link)`
-    cursor: pointer;
-    height: 30px;
-    line-height: 30px;
-    color: white;
-    background-color: red;
-    text-decoration: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 16px;
-    margin: 0;
-    padding: 0 10px;
-    font-weight: 700;
-`;
-
-const LinkContainer = styled.div`
-    display: flex;
-    padding: 0 5%;
-    justify-content: space-between;
-    margin-top: 10px;
-`;
 
 export default Detail;
