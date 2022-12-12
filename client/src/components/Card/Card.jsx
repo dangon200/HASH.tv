a2546ca2b5779c8c428ca3095ceb923da0fc53d1
Agustin Fraile <agustinfraile98@gmail.com>
2022-12-12 12:55:16 -0300

    cambiando la fuente

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import '../Card/Card.css'
 import { useSelector } from 'react-redux';
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import '../Card/Card.css'
 
 function Card() {
     const streams = useSelector(state => state.streams)
