2f78a30806b02662e7065098840599065cdb3ebe
René Valderrey González <rene_valderrey@hotmail.com>
2022-12-14 18:32:17 -0300

    fe de erratas:

--- client/src/Pages/Support.jsx
+++ client/src/Pages/Support.jsx
@@ -18,7 +18,7 @@ function Support() {
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header style={{ width: '97%' }}>¿Cuándo es indicado hacer streaming?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               Antes de pensar si vale la pena usar este recurso, tratá de ponerte en el lugar de los compradores e imaginá si te interesaría asistir a esa transmisión. Por eso, invertí en un contenido que genere valor e incentivá a tus clientes a participar en él.
 
               Podés hacer streaming para divulgar el lanzamiento de una nueva colección, promociones o, inclusive, una entrevista para conocer mejor a tus clientes. Recordá que es fundamental ofrecer informaciones relevantes y de calidad para los espectadores.
@@ -28,37 +28,37 @@ function Support() {
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header style={{ width: '97%' }}>¿Qué quiere decir en streaming?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               Streaming significa visualización o transmisión en directo y se refiere al envío de datos en un flujo continuo, sin tener que esperar a que se descargue el contenido.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="3">
             <Accordion.Header style={{ width: '97%' }}>¿Cómo se puede ver un streaming?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               En líneas generales, se accede a un streaming a través de un enlace (link) único. También podés ver una transmisión en vivo desde el perfil de una persona o una marca dentro de una red social como HASH.tv!
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="4">
             <Accordion.Header style={{ width: '97%' }}>¿Qué es ser un streamer?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               Un streamer es la persona que realiza transmisiones en vivo a través de las plataformas ideadas para esto.
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="4">
+          <Accordion.Item eventKey="5">
             <Accordion.Header style={{ width: '97%' }}>¿A qué hora hacer streaming?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               Las mejores horas para hacer streaming en 2022 son de 3:00 pm a 6:00 pm (hora Argentina) y los mejores días son sábados y domingos. Así lo reportó la reconocida empresa de análisis de redes sociales Hootsuite. Sin embargo, te recomendamos que investigues en qué horarios suele estar más activa tu audiencia.
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="5">
+          <Accordion.Item eventKey="6">
             <Accordion.Header style={{ width: '97%' }}>¿Cómo registrarse en HASH.tv?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               Simple. Ingresa en el botón de arriba a la derecha "Iniciar Sesión", luego da click en "Crear Usuario". Podrás crear un usuario con el correo electrónico que desees o iniciando sesión con Google.
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="6">
+          <Accordion.Item eventKey="7">
             <Accordion.Header style={{ width: '97%' }}>¿Cómo suscribirse a un canal?</Accordion.Header>
-            <Accordion.Body>
+            <Accordion.Body style={{ background: 'rgba(217, 217, 217, 0.6)' }}>
               El sistema de suscripciones se maneja con una moneda digital denominada "HashCash". Para adquirir tu HashCash podés dirigirte hacia tu perfil de usuario y comprar mediante MercadoPago.
             </Accordion.Body>
           </Accordion.Item>
@@ -70,6 +70,7 @@ function Support() {
 
 const Container = s.div`
   background: #404040;
+  height: 580px;
 `
 const Title = s.h2`
   text-align: left;
