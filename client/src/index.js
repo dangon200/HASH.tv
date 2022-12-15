ded10ccdcd7ea3d05f203e43962cdc3f17de3355
dangon200 <gonzalezdaniel230@gmail.com>
2022-12-15 12:12:52 -0300

    asdsdas

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 // import { SocketContext, socket } from './context/socket'
 import { Provider } from "react-redux";
